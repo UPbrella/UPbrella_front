ff5db600d0d9d906d15bd3e17abf6af7feb6329b
ShinChanU <tlscksdn963@naver.com>
2023-07-10 22:43:43 +0900

    chore: pr 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -20,3 +20,14 @@ function App() {
 }
 
 export default App;
+/*
+https://docs.google.com/presentation/d/17fncEhQqegrwfyPdS6wuoISsSGM0-scDEIq2JLwSYLU/edit?pli=1#slide=id.g22ef3d693d6_0_3
+
+1. 대여 반납 현황 조회 : 엑셀 다운로드
+2. 우산 관리
+3. 대여폼 상태신고, 반납폼 개선사항
+4. 협업 지점 관리
+mui, atomic, storybook
+협업지점 목록 조회 - 테이블
+추가 및 수정 - 모달
+*/
