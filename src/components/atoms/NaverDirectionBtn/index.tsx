3b6a1bfa6189f97d6005b64b110bece39357a788
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-07-18 15:00:02 +0900

    feat: 네이버 길찾기 버튼

--- src/components/atoms/NaverDirectionBtn/index.tsx
+++ src/components/atoms/NaverDirectionBtn/index.tsx
@@ -2,7 +2,7 @@ import NearMeOutlinedIcon from "@mui/icons-material/NearMeOutlined";
 
 const NaverDirectionBtn = () => {
   return (
-    <div className="flex items-center	ali">
+    <div className="flex items-center">
       <button className="text-main-color bg-sub-color rounded-99 w-360 h-40 text-15 font-semibold	justify-items-center	">
         <NearMeOutlinedIcon className="mr-2"></NearMeOutlinedIcon>네이버 길찾기
       </button>
