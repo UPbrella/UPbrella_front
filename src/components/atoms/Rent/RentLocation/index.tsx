dc94f078afa2c2fe0ad14f8caac4bd6a075f4497
heehminh <mink141416@gmail.com>
2023-07-19 00:43:38 +0900

    feat: rent form atom

--- src/components/atoms/Rent/RentLocation/index.tsx
+++ src/components/atoms/Rent/RentLocation/index.tsx
@@ -7,6 +7,8 @@ export type RentLocationProps = {
 const RentLocation = ({ label, isTitle, location }: RentLocationProps) => {
   const width = isTitle ? "96px" : "216px";
 
+  // TODO 미리 입력받고 값이 정해져있는 경우엔 디자인 수정이 필요할듯함
+
   return (
     <div className="flex-col w-330 p-5">
       <div className="flex items-center mb-4 text-gray-700 text-15 leading-22 font-normal mr-4">
