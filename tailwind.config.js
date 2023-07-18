12e840f8cba82065b04bd93155ce3352d7171363
heehminh <mink141416@gmail.com>
2023-07-18 23:46:59 +0900

    fix: confilct 해결

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,17 +18,16 @@ export default {
         "sub-color": "#FEF4F2",
         "primary-700": "#E05938",
         "primary-600": "#E86546",
-        "primary-500": "#E86F52",
+
         "primary-400": "#FCE7E3",
         "primary-300": "#FCEDEA",
-        "primary-200": "#FEF4F2",
+
         "primary-100": "#FFFCFC",
         black: "#111111",
-        "gray-700": "#333333",
-        "gray-600": "#666666",
+
         "gray-500": "#999999",
         "gray-400": "#CCCCCC",
-        "gray-300": "#DDDDDD",
+
         "gray-200": "#EEEEEE",
         "gray-100": "#F8F8FA",
         red: "#FF0000",
