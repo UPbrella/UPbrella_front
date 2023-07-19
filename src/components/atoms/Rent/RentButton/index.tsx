66de5b585fb47600b03ff8a10d44af98b4c2fcee
heehminh <mink141416@gmail.com>
2023-07-19 20:41:41 +0900

    style: button background color 수정

--- src/components/atoms/Rent/RentButton/index.tsx
+++ src/components/atoms/Rent/RentButton/index.tsx
@@ -1,10 +1,12 @@
+import React from "react";
+
 export type RentButtonProps = {
   label: string;
 };
 
 const RentButton = ({ label }: RentButtonProps) => {
   return (
-    <button className="w-320 h-48 bg-primary-600 rounded-8 text-white text-16 leading-24 font-semibold">
+    <button className="w-320 h-48 bg-primary-500 rounded-8 text-white text-16 leading-24 font-semibold">
       {label}
     </button>
   );
--- src/components/atoms/Rent/RentLocation/index.tsx
+++ src/components/atoms/Rent/RentLocation/index.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export type RentLocationProps = {
   label: string;
   isTitle: boolean;
