a466b72cf16bec85a15fa51749c5cdb92515b7a8
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2023-07-26 21:56:57 +0900

    fix: tailwindcss width, height

--- src/components/atoms/Header/HeaderLogo.tsx
+++ src/components/atoms/Header/HeaderLogo.tsx
@@ -1,5 +1,5 @@
 import Logo from "@/assets/main_logo.svg";
 
 export const HeaderLogo = () => {
-  return <img className="w-[48px] h-[48px]" src={Logo} />;
+  return <img className="w-48 h-48" src={Logo} />;
 };
