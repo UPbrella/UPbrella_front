c0621af5777e3975d5ad9aef707ccd5d04331b8f
heehminh <mink141416@gmail.com>
2023-08-12 04:50:47 +0900

    feat: rent form modal

--- src/components/atoms/Form/FormButton/index.tsx
+++ src/components/atoms/Form/FormButton/index.tsx
@@ -1,11 +1,15 @@
 export type FormButtonProps = {
   label: string;
+  handleOpen?: () => void;
 };
 
-const FormButton = ({ label }: FormButtonProps) => {
+const FormButton = ({ label, handleOpen }: FormButtonProps) => {
   return (
     <div className="max-w-2xl">
-      <button className="w-full h-48 bg-primary-500 rounded-8 text-white text-16 leading-24 font-semibold">
+      <button
+        className="w-full h-48 bg-primary-500 rounded-8 text-white text-16 leading-24 font-semibold"
+        onClick={handleOpen}
+      >
         {label}
       </button>
     </div>
