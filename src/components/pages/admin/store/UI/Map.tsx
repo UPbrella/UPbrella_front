4b9d71074588c3cc65ad401380df135438d548ee
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-08-13 19:06:27 +0900

    fix: 기본 map 컴포넌트에 radius 속성 옵션 추가

--- src/components/pages/admin/store/UI/Map.tsx
+++ src/components/pages/admin/store/UI/Map.tsx
@@ -3,14 +3,15 @@ import { forwardRef } from "react";
 type TProps = {
   width?: string;
   height?: string;
+  borderRadius?: string;
 };
 
 // 네이버 map 으로 사용할 컴포넌트
 const Map = forwardRef<HTMLDivElement, TProps>(function Map(
-  { width = "500px", height = "60vh" }: TProps,
+  { width = "100%", height = "60vh", borderRadius = "0px" }: TProps,
   ref
 ) {
-  return <div id="map" ref={ref} style={{ width, height }}></div>;
+  return <div id="map" ref={ref} style={{ width, height, borderRadius }}></div>;
 });
 
 export default Map;
