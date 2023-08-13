5c2ff2c251438c3481a4fc228c252489f97ae429
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-08-13 19:17:15 +0900

    feat: 유저 현재위치 받아오는 함수

---
+++ src/utils/map/mapHelpers.tsx
@@ -0,0 +1,9 @@
+export const getCurrentPosition = (): Promise<GeolocationPosition> => {
+  return new Promise((resolve, reject) => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    } else {
+      reject(new Error("Geolocation not available"));
+    }
+  });
+};
