efff83372774e1a450ff20f827dbb469d391cb9f
heehminh <mink141416@gmail.com>
2023-08-16 00:39:03 +0900

    feat: bottomsheet additional function

--- src/components/atoms/BottomSheet/index.tsx
+++ src/components/atoms/BottomSheet/index.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import Sheet, { SheetRef } from "react-modal-sheet";
 
 export type BottomSheetProps = {
@@ -7,21 +7,45 @@ export type BottomSheetProps = {
   setIsBottomSheetOpen: (value: boolean) => void;
 };
 
+const snapPoints = [484, 272, 0];
+
 const BottomSheet = ({ children, isBottomSheetOpen, setIsBottomSheetOpen }: BottomSheetProps) => {
   const ref = useRef<SheetRef>();
+  const contentRef = useRef<HTMLDivElement>(null);
+  const snapTo = (i: number) => ref.current?.snapTo(i);
+
+  // 외부 클릭 시 바텀시트 닫기
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (!contentRef.current?.contains(event.target as Node)) {
+      setIsBottomSheetOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isBottomSheetOpen) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    } else {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  });
 
   return (
     <Sheet
       ref={ref}
       isOpen={isBottomSheetOpen}
       onClose={() => setIsBottomSheetOpen(false)}
-      snapPoints={[484, 272, 100, 0]}
+      snapPoints={snapPoints}
       initialSnap={1}
     >
       <Sheet.Container>
         <Sheet.Header />
         <Sheet.Content>
-          <div style={{ overflow: "scroll" }}>{children}</div>
+          <div style={{ overflow: "scroll" }} onScroll={() => snapTo(0)}>
+            {children}
+          </div>
         </Sheet.Content>
       </Sheet.Container>
     </Sheet>
