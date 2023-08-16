926041454d0762918490b3fb65910b1a186e3e78
LeeJSYS <dauzzang@gmail.com>
2023-08-16 14:38:38 +0900

    Style: Login Page 높이 가득 채우기 문제 해결

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
           },
         }}
       />
-      <div className="max-w-[1440px] px-40 mx-auto">
+      <div className="max-w-[1440px] min-h-[100vh] px-40 mx-auto flex flex-col">
         <Routes>
           {/* Not Found Page */}
 
