bda5ceab2d32eccea92b311f0e3dd5b266ce26a6
Clay ChanWoo <claychanwoo@naver.com>
2023-09-03 20:22:49 +0900

    feat: 어드민 > 회원 조회 및 삭제 API 코드 추가

---
+++ src/api/userApi.ts
@@ -0,0 +1,19 @@
+import { $axios } from "@/lib/axios";
+import { TUserRes } from "@/types/admin/userTypes";
+import { TApiResponse } from "@/types/commonTypes";
+
+const API = {
+  GET_USERS: () => `/users`,
+  DELETE_USERS: (userId: number) => `/users/${userId}`,
+} as const;
+
+// 유저 전체 조회
+export const getUsers = async () => {
+  const res = await $axios.get<TApiResponse<{ users: TUserRes[] }>>(API.GET_USERS());
+  return res.data;
+};
+
+// 유저 삭제
+export const deleteUsers = async (userId: number) => {
+  await $axios.delete(API.DELETE_USERS(userId));
+};
