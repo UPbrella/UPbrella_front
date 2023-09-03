14fb51b81b9f91607a156693dae51c88349af8bd
Clay ChanWoo <claychanwoo@naver.com>
2023-09-03 23:11:36 +0900

    feat: 회원관리 페이지 라우터 추가

--- src/components/templates/admin/AdminMenu.tsx
+++ src/components/templates/admin/AdminMenu.tsx
@@ -1,16 +1,12 @@
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Tab, Tabs } from "@mui/material";
+import { ADMIN_ROUTES_URL } from "@/routes/adminRouter";
 
 const adminMenu = [
   {
-    title: "협업 지점 관리",
-    navToUrl: "/admin/stores",
-    disabled: false,
-  },
-  {
-    title: "대여 / 반납 조회",
-    navToUrl: "/admin/rent-history",
+    title: "대여/반납 현황 테이블",
+    navToUrl: ADMIN_ROUTES_URL.rent.path,
     disabled: false,
   },
   {
@@ -19,9 +15,19 @@ const adminMenu = [
     disabled: true,
   },
   {
+    title: "회원 관리",
+    navToUrl: ADMIN_ROUTES_URL.user.path,
+    disabled: false,
+  },
+  {
     title: "상태신고 / 개선사항 확인",
     navToUrl: "/admin/umbrella-status",
     disabled: true,
+  },
+  {
+    title: "협업 지점 관리",
+    navToUrl: ADMIN_ROUTES_URL.store.path,
+    disabled: false,
   },
 ];
 
@@ -45,7 +51,7 @@ const AdminMenu = () => {
 
   return (
     <>
-      <Tabs value={value} onChange={(_, value) => handleChange(value)}>
+      <Tabs className="mt-16" value={value} onChange={(_, value) => handleChange(value)}>
         {adminMenu.map(({ title, disabled }) => (
           <Tab key={title} label={title} disabled={disabled} />
         ))}
