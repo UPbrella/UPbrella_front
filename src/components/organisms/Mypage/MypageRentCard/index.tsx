0e1fd0c87589dbe191833c3a9646f4e947e431df
LeeJSYS <dauzzang@gmail.com>
2023-09-08 19:41:54 +0900

    style: mypage 이용내역 CSS full width로 확장

--- src/components/organisms/Mypage/MypageRentCard/index.tsx
+++ src/components/organisms/Mypage/MypageRentCard/index.tsx
@@ -6,9 +6,9 @@ export type MypageRentCardProps = {
 
 const MypageRentCard = ({ rentList }: MypageRentCardProps) => {
   return (
-    <section className="py-24">
+    <section className="py-24 flex flex-col flex-1">
       <div className="text-32 font-semibold leading-40 text-black mb-24">이용 내역</div>
-      <div>
+      <div className="flex-1">
         <MypageRentList rentList={rentList} />
       </div>
     </section>
