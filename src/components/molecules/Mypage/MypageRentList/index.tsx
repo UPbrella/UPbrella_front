d0b18750bc043d59322312df21a905d34dffb6c3
LeeJSYS <dauzzang@gmail.com>
2023-09-08 20:22:33 +0900

    style: mypage 이용내역 첫번째 내역 background color 적용되게 수정

--- src/components/molecules/Mypage/MypageRentList/index.tsx
+++ src/components/molecules/Mypage/MypageRentList/index.tsx
@@ -19,16 +19,15 @@ const MypageRentList = ({ rentList }: MypageRentListProps) => {
     <section className="flex flex-col flex-1">
       {rentList.map((rent, index) => {
         if (index === rentList.length - 1) {
-          if (index === 0) {
-            return (
-              <div key={index} className="flex flex-1">
-                <MypageRentSection isProfile={false} isRecent={true} rentInfo={rent} />
-              </div>
-            );
-          }
           return (
             <div key={index} className="flex flex-1">
               <MypageRentSection isProfile={false} isRecent={false} rentInfo={rent} />
+            </div>
+          );
+        } else if (index === 0) {
+          return (
+            <div key={index} className="flex flex-1 mb-16">
+              <MypageRentSection isProfile={false} isRecent={true} rentInfo={rent} />
             </div>
           );
         }
