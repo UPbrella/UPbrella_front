7962a2d25ba0642d114830986d7c6b026b15e8e9
LeeJSYS <dauzzang@gmail.com>
2023-09-11 10:35:09 +0900

    feat: 로그인 상태 관리, 대여내역 상태관리 기능 완성

--- src/recoil/index.ts
+++ src/recoil/index.ts
@@ -1,11 +1,36 @@
 import { $axios } from "@/lib/axios";
-import { selector } from "recoil";
+import { atom, selector } from "recoil";
 
+export type TUserInfo = {
+  id: number;
+  name: string;
+  phoneNumber: string;
+  bank?: string;
+  accountNumber?: string;
+};
+
+export const userInfo = atom<TUserInfo>({
+  key: "userInfo",
+  default: {
+    id: 0,
+    name: "",
+    phoneNumber: "",
+    bank: "",
+    accountNumber: "",
+  },
+});
 export const loginInfo = selector({
   key: "loginInfo",
   get: async () => {
     const response = await $axios.get("/users/loggedIn", { withCredentials: true });
-
     return response.data.data;
+  },
+});
+
+export const rentHistories = selector({
+  key: "rentHistories",
+  get: async () => {
+    const response = await $axios.get("/users/histories", { withCredentials: true });
+    return response.data.data.histories;
   },
 });
