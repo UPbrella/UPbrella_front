66fdd429c7549c7ee62071f4add36dd35451d805
heehminh <mink141416@gmail.com>
2023-09-16 19:19:24 +0900

    feat: loading

--- src/components/pages/rent/index.tsx
+++ src/components/pages/rent/index.tsx
@@ -27,6 +27,8 @@ const RentPage = () => {
   const umbrellaId = id ? parseInt(id, 10) : 0;
   const navigate = useNavigate();
 
+  const [isLoading, setIsLoading] = useState(false);
+
   // 대여폼
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
@@ -46,8 +48,13 @@ const RentPage = () => {
   }, [userInfo]);
 
   // 대여 폼 데이터 조회 (location, storeName, umbrellaNo)
-  const { data, isError } = useGetRentFormData(umbrellaId);
+  const { data, isError, isLoading: rentFormDataLoading } = useGetRentFormData(umbrellaId);
+  if (rentFormDataLoading) {
+    setIsLoading(true);
+  }
+
   if (isError) {
+    setIsLoading(false);
     // TODO Not-found URL 형식
     navigate("/404");
     toast.error("존재하지 않는 우산 고유 번호입니다.");
@@ -61,6 +68,7 @@ const RentPage = () => {
 
   useEffect(() => {
     if (data) {
+      setIsLoading(false);
       setRegion(data.classificationName);
       setStoreName(data.rentStoreName);
       setUmbrellaUuid(data.umbrellaUuid);
@@ -99,7 +107,9 @@ const RentPage = () => {
   const [isOpenLockPwModal, setIsOpenLockPwModal] = useState(false); // 자물쇠 비밀번호 안내 모달
   const handleCloseLockPwModal = () => setIsOpenLockPwModal(false);
 
-  return (
+  return isLoading ? (
+    <div>Loading...</div>
+  ) : (
     <div className="flex-col max-w-2xl mx-auto">
       <MobileHeader />
       <div className="mt-20 text-24 font-semibold leading-32 text-black">
