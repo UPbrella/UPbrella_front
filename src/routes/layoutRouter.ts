45cddfc4a2d5117e7732172259a5032f2826b68e
LeeJSYS <dauzzang@gmail.com>
2023-09-17 16:46:13 +0900

    style: 개인정보조회페이지 path추가

--- src/routes/layoutRouter.ts
+++ src/routes/layoutRouter.ts
@@ -7,6 +7,7 @@ import LoginPage from "@/components/pages/Login/LoginPage";
 import LoginRedirect from "@/components/pages/Login/LoginRedirectPage";
 import MypageRentPage from "@/components/pages/Mypage/MypageRentPage";
 import MypageAccountPage from "@/components/pages/Mypage/MypageAccountPage";
+import MypageInfoPage from "@/components/pages/Mypage/MypageInfoPage";
 
 /**
  * Header, footer의 layout이 필요한 페이지
@@ -52,5 +53,10 @@ export const LAYOUT_ROUTES: TRoute[] = [
     name: "마이페이지_계좌등록변경",
     path: "/members/mypage/account",
     component: MypageAccountPage,
+  },
+  {
+    name: "마이페이지_개인정보조회",
+    path: "/members/mypage/info",
+    component: MypageInfoPage,
   },
 ];
