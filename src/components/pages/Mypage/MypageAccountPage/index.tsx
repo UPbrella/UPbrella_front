9d069fb42bb9903d9361e03a34fe5cf4b8ec0656
LeeJSYS <dauzzang@gmail.com>
2023-09-17 16:30:49 +0900

    feat: 계좌 등록, 변경, 삭제 기능 추가

--- src/components/pages/Mypage/MypageAccountPage/index.tsx
+++ src/components/pages/Mypage/MypageAccountPage/index.tsx
@@ -1,6 +1,9 @@
 import MypageAccountCard from "@/components/organisms/Mypage/MypageAccountCard";
 import MypageLeftCard from "@/components/organisms/Mypage/MypageLeftCard";
+import { $axios } from "@/lib/axios";
+import { loginInfo } from "@/recoil";
 import { ChangeEvent, MouseEvent, useEffect, useRef, useState } from "react";
+import { useRecoilValueLoadable } from "recoil";
 
 export type TAccountPageInputs = {
   bank: string;
@@ -13,22 +16,38 @@ const MypageAccountPage = () => {
     accountNumber: "",
   });
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
-  const [isInputCompleted, setIsInputCompleted] = useState<boolean>(false);
+  const [hasBankAccountInfo, setHasBankAccountInfo] = useState<boolean>(false);
 
+  const loginInfoValue = useRecoilValueLoadable(loginInfo);
   const bankInput = useRef<HTMLInputElement>(null);
 
   const { bank, accountNumber } = inputs;
 
   useEffect(() => {
-    const handleRegisterButtonActivated = () => {
-      if (bank !== "" && accountNumber !== "") {
-        setIsInputCompleted(true);
-      } else {
-        setIsInputCompleted(false);
+    const getBankAccountInfo = async () => {
+      switch (loginInfoValue.state) {
+        case "hasValue":
+          if (loginInfoValue.contents.bank) {
+            const data = {
+              ["bank"]: loginInfoValue.contents.bank,
+              ["accountNumber"]: loginInfoValue.contents.accountNumber,
+            };
+            setInputs({ ...data });
+            setHasBankAccountInfo(true);
+          } else {
+            setHasBankAccountInfo(false);
+          }
+          break;
+        case "loading":
+          <div>Loading...</div>;
+          break;
+        case "hasError":
+          break;
       }
     };
-    handleRegisterButtonActivated();
-  }, [bank, accountNumber]);
+
+    getBankAccountInfo();
+  }, [bank, accountNumber, loginInfoValue.state, loginInfoValue.contents]);
   const handleInputValue = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setInputs({ ...inputs, [name]: value });
@@ -50,6 +69,33 @@ const MypageAccountPage = () => {
     }
   };
 
+  const handleDeleteAccount = async () => {
+    await $axios.delete("/users/bankAccount", { withCredentials: true }).then(() => {
+      const data = {
+        ["bank"]: "",
+        ["accountNumber"]: "",
+      };
+      setInputs({ ...data });
+      setHasBankAccountInfo(false);
+      alert("계좌 삭제 완료!");
+    });
+  };
+  //   계좌 등록과 수정 모두 같은 patch api 요청
+  const handleChangeAccount = async () => {
+    await $axios.patch("/users/bankAccount", { ...inputs }, { withCredentials: true }).then(() => {
+      setInputs({ ...inputs });
+      setHasBankAccountInfo(true);
+      alert("계좌 변경 완료!");
+    });
+  };
+  const handleRegisterAccount = async () => {
+    await $axios.patch("/users/bankAccount", { ...inputs }, { withCredentials: true }).then(() => {
+      setInputs({ ...inputs });
+      setHasBankAccountInfo(true);
+      alert("계좌 등록 완료!");
+    });
+  };
+
   return (
     <div className="flex justify-center w-[1280px] mt-24 px-40">
       <div className="flex flex-col w-full">
@@ -68,8 +114,11 @@ const MypageAccountPage = () => {
               isOpenModal={isOpenModal}
               handleClose={handleClose}
               handleClickBank={handleClickBank}
-              hasBankAccountInfo={false}
-              isInputCompleted={isInputCompleted}
+              hasBankAccountInfo={hasBankAccountInfo}
+              isInputCompleted={bank !== "" && accountNumber !== ""}
+              onClickDeleteButton={handleDeleteAccount}
+              onClickChangeButton={handleChangeAccount}
+              onClickRegisterButton={handleRegisterAccount}
             />
           </div>
         </div>
