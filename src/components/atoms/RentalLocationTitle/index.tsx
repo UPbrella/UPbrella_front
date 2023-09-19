46dbbcb2462229165f18cb887ea164131fe302ae
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-09-19 15:30:16 +0900

    fix: categoty 타입 변경

--- src/components/atoms/RentalLocationTitle/index.tsx
+++ src/components/atoms/RentalLocationTitle/index.tsx
@@ -1,13 +1,13 @@
 export type TRentalLocationTitle = {
   title: string;
-  category: string[];
+  category: string;
 };
 
 const RentalLocationTitle = ({ title, category }: TRentalLocationTitle) => {
   return (
     <div className="flex items-center justify-center">
       <div className="text-2xl font-bold mr-8">{title}</div>
-      <div className="text-sm text-gray-600">{category.join(", ")}</div>
+      <div className="text-sm text-gray-600">{category}</div>
     </div>
   );
 };
