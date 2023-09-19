a1d5f6a49ea40ba0f1a2f64d717c5432ff9d38c1
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-09-20 00:44:27 +0900

    feat: 반응형에 따른 모바일 카드

--- src/components/organisms/ImgSwiper/index.tsx
+++ src/components/organisms/ImgSwiper/index.tsx
@@ -18,7 +18,7 @@ const ImgSwiper = ({ maxWidth, maxHeight, images }: TImgSwiper) => {
     maxHeight: maxHeight ? `${maxHeight}px` : "100%", // maxHeight props에 따라 설정
   };
   return (
-    <div className="flex justify-start" style={swiperStyles}>
+    <div className="flex justify-start w-full h-full" style={swiperStyles}>
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y]}
         spaceBetween={10}
--- src/components/pages/rentalOffice/RentalOfficePage.tsx
+++ src/components/pages/rentalOffice/RentalOfficePage.tsx
@@ -13,38 +13,57 @@ const RentalOfficePage = () => {
   const [selectedStoreId, setSelectedStoreId] = useState<number | null>(null);
   const [, setSelectedClassificationId] = useState<number | null>(null);
   const [selectedClassificationName, setSelectedClassificationName] = useState<string>("");
+  const [isMobileCardOpen, setIsMobileCardOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   // server
   const { data: subClassificationsRes } = useGetSubClassifications();
   const { data: storeListRes } = useGetStoreList();
   const { data: useGetStoreDetailData } = useGetStoreDetail(selectedStoreId ?? 1);
 
+  // Mobile Card 닫기
+  // const closeMobileCard = () => {
+  //   setIsMobileCardOpen(false);
+  // };
   return (
-    <div className="flex mt-24">
-      <div className="flex mr-24 md:hidden">
+    <div className="flex justify-center mt-24 md:flex-col sm:px-20">
+      <div className="flex mr-24 md:hidden lg:hidden">
         {useGetStoreDetailData && <Card storeDetail={useGetStoreDetailData} />}
       </div>
       <div>
-        {subClassificationsRes && (
-          <LocationClassificationBtn
-            classifications={subClassificationsRes}
-            setSelectedClassificationId={setSelectedClassificationId}
-            setSelectedClassificationName={setSelectedClassificationName}
-          />
+        {subClassificationsRes && !isMobileCardOpen && (
+          <div className="md:hidden">
+            <LocationClassificationBtn
+              classifications={subClassificationsRes}
+              setSelectedClassificationId={setSelectedClassificationId}
+              setSelectedClassificationName={setSelectedClassificationName}
+            />
+          </div>
         )}
         <div>
-          {storeListRes && (
+          {storeListRes && !isMobileCardOpen && (
             <div>
               <Store
                 storeList={storeListRes}
+                setIsMobileCardOpen={setIsMobileCardOpen}
                 classifications={subClassificationsRes}
                 setSelectedStoreId={setSelectedStoreId}
                 selectedClassificationName={selectedClassificationName}
+                setIsMobile={setIsMobile}
               />
             </div>
           )}
         </div>
       </div>
+      {isMobileCardOpen && useGetStoreDetailData && (
+        <div className="flex justify-center items-center mr-8 xl:hidden">
+          <Card
+            storeDetail={useGetStoreDetailData}
+            maxWidth={isMobile ? 320 : 600}
+            maxHeight={isMobile ? 224 : 420}
+          />
+        </div>
+      )}
     </div>
   );
 };
