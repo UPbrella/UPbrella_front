fad1e7a3b34d1a3fb58923b00e731700df7506b6
heehminh <mink141416@gmail.com>
2023-09-21 09:42:07 +0900

    feat: content가 null인 경우 필터링

--- src/components/pages/admin/feedback/UmbrellaFeedBackPage.tsx
+++ src/components/pages/admin/feedback/UmbrellaFeedBackPage.tsx
@@ -13,11 +13,20 @@ const UmbrellaFeedBackPage = () => {
   // 개선 요청 내역 조회
   const { data: improvementReportsRes } = useGetHistoriesImprovements();
 
+  // 'content'가 null이 아닌 항목만 필터링
+  const filteredConditionReports = conditionReportsRes?.conditionReports.filter(
+    (report) => report.content !== ""
+  );
+
+  const filteredImprovementReports = improvementReportsRes?.improvementReports.filter(
+    (report) => report.content !== ""
+  );
+
   return (
     <div>
       <div>
         <Typography>신고 내역 조회 - 대여폼</Typography>
-        <CssDataTable value={conditionReportsRes?.conditionReports}>
+        <CssDataTable value={filteredConditionReports}>
           <Column field="id" header="ID" />
           <Column field="umbrellaUuid" header="umbrellaUuid" />
           <Column field="content" header="content" />
@@ -26,7 +35,7 @@ const UmbrellaFeedBackPage = () => {
 
       <div>
         <Typography>개선 요청 내역 조회 - 반납폼</Typography>
-        <CssDataTable value={improvementReportsRes?.improvementReports}>
+        <CssDataTable value={filteredImprovementReports}>
           <Column field="id" header="ID" />
           <Column field="umbrellaUuid" header="umbrellaUuid" />
           <Column field="content" header="content" />
