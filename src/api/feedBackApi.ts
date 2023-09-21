72b5cf644c3f111f33330c86bb8f09f9e04e40f5
heehminh <mink141416@gmail.com>
2023-09-21 09:33:28 +0900

    feat: 신고내역조회, 개선요청내역조회

---
+++ src/api/feedBackApi.ts
@@ -0,0 +1,24 @@
+import { $axios } from "@/lib/axios";
+import { TApiResponse } from "@/types/commonTypes";
+import { TConditionRes, TImprovementRes } from "@/types/admin/FeedbackTypes";
+
+const API = {
+  HISTORIES_STATUS: () => `/admin/rent/histories/status`,
+  HISTORIES_IMPROVEMENTS: () => `/admin/rent/histories/improvements`,
+} as const;
+
+// 신고 내역 조회
+export const getHistoriesStatus = async () => {
+  const res = await $axios.get<TApiResponse<TConditionRes>>(API.HISTORIES_STATUS(), {
+    withCredentials: true,
+  });
+  return res.data;
+};
+
+// 개선 요청 내역 조회
+export const getHistoriesImprovements = async () => {
+  const res = await $axios.get<TApiResponse<TImprovementRes>>(API.HISTORIES_IMPROVEMENTS(), {
+    withCredentials: true,
+  });
+  return res.data;
+};
--- src/components/pages/admin/feedback/UmbrellaFeedBackPage.tsx
+++ src/components/pages/admin/feedback/UmbrellaFeedBackPage.tsx
@@ -1,5 +1,39 @@
+import { Typography } from "@mui/material";
+import { Column } from "primereact/column";
+import {
+  useGetHistoriesImprovements,
+  useGetHistoriesStatus,
+} from "@/hooks/queries/feedbackQueries";
+import { CssDataTable } from "../components/Table";
+
 const UmbrellaFeedBackPage = () => {
-  return <div></div>;
+  // 신고 내역 조회
+  const { data: conditionReportsRes } = useGetHistoriesStatus();
+
+  // 개선 요청 내역 조회
+  const { data: improvementReportsRes } = useGetHistoriesImprovements();
+
+  return (
+    <div>
+      <div>
+        <Typography>신고 내역 조회 - 대여폼</Typography>
+        <CssDataTable value={conditionReportsRes?.conditionReports}>
+          <Column field="id" header="ID" />
+          <Column field="umbrellaUuid" header="umbrellaUuid" />
+          <Column field="content" header="content" />
+        </CssDataTable>
+      </div>
+
+      <div>
+        <Typography>개선 요청 내역 조회 - 반납폼</Typography>
+        <CssDataTable value={improvementReportsRes?.improvementReports}>
+          <Column field="id" header="ID" />
+          <Column field="umbrellaUuid" header="umbrellaUuid" />
+          <Column field="content" header="content" />
+        </CssDataTable>
+      </div>
+    </div>
+  );
 };
 
 export default UmbrellaFeedBackPage;
