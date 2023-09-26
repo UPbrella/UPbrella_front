436513c09bf67333f77d41cb79daa751a47cabd7
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-09-27 01:49:59 +0900

    style: 버튼 padding 조절

--- src/components/pages/Info/InfoPage.tsx
+++ src/components/pages/Info/InfoPage.tsx
@@ -5,6 +5,8 @@ import HowRent from "@/components/organisms/Info/HowRent";
 import HowReturn from "@/components/organisms/Info/HowReturn";
 import FAQ from "@/components/organisms/Info/FAQ";
 import Footer from "@/components/organisms/Footer";
+import { FixWidthWrapper } from "../story/UpbrellaStoryPage";
+import { HeaderContainer } from "@/components/organisms/Header/HeaderContainer";
 
 const mock: TClassification[] = [
   {
@@ -39,18 +41,25 @@ const InfoPage = () => {
 
   return (
     <>
-      <div className="px-120">
-        <div className="font-semibold text-24 pb-32 pt-24">이용안내</div>
-        <LocationClassificationBtn
-          classifications={mock}
-          setSelectedClassificationId={setSelectedClassificationIndex}
-          handleClassificationSelection={handleClassificationSelection}
-        />
-      </div>
+      <FixWidthWrapper>
+        <HeaderContainer />
+        <div className="flex items-center lg:w-680 md:px-10">
+          <div className="flex flex-1 flex-col justify-center px-120 sm:px-0 smMaxLg:px-0">
+            <div className="font-semibold text-24 pb-32 pt-24">이용안내</div>
+            <LocationClassificationBtn
+              classifications={mock}
+              setSelectedClassificationId={setSelectedClassificationIndex}
+              handleClassificationSelection={handleClassificationSelection}
+            />
+          </div>
+        </div>
+      </FixWidthWrapper>
       {selectedClassificationIndex === 0 && <HowRent />}
       {selectedClassificationIndex === 1 && <HowReturn />}
       {selectedClassificationIndex === 2 && <FAQ />}
-      <Footer />
+      <FixWidthWrapper>
+        <Footer />
+      </FixWidthWrapper>
     </>
   );
 };
