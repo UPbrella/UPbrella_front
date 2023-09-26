dba91c3abd8b71865dbf30018fc340c0b31f91d4
LeeJSYS <dauzzang@gmail.com>
2023-09-26 17:19:03 +0900

    feat: 페이지에 맞게 nav수정

--- src/components/molecules/Mypage/MypageNav/index.tsx
+++ src/components/molecules/Mypage/MypageNav/index.tsx
@@ -1,9 +1,10 @@
 import { ExpandLess, ExpandMore } from "@mui/icons-material";
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const MypageNav = () => {
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  const location = useLocation();
   const navList = [
     {
       name: "이용내역",
@@ -22,6 +23,14 @@ const MypageNav = () => {
       path: "/members/mypage/contact",
     },
   ] as const;
+  const navRepresent = navList.find((nav) => nav.path === location.pathname);
+  const navDropDown = navList.filter((nav) => {
+    if (nav.path !== location.pathname) {
+      return true;
+    } else {
+      return false;
+    }
+  });
 
   const onClickExpandButton = () => {
     setIsExpanded(!isExpanded);
@@ -58,7 +67,7 @@ const MypageNav = () => {
           className="h-48 px-20 flex justify-between items-center rounded-8 bg-primary-300 mb-8"
           onClick={onClickExpandButton}
         >
-          <p className="text-black text-18 font-semibold leading-24">{navList[0].name}</p>
+          <p className="text-black text-18 font-semibold leading-24">{navRepresent?.name}</p>
           {isExpanded ? (
             <ExpandLess className="text-gray-700" />
           ) : (
@@ -68,28 +77,24 @@ const MypageNav = () => {
         <div className="relative">
           {isExpanded ? (
             <div className="border border-solid border-gray-200 rounded-8 shadow-md absolute left-0 right-0">
-              {navList.map((navInfo, index) => {
-                if (index !== 0) {
-                  return (
-                    <div>
-                      {isExpanded ? (
-                        <NavLink
-                          key={navInfo.name}
-                          to={navInfo.path}
-                          className="w-full h-48 px-20 flex items-center bg-white rounded-8"
-                        >
-                          <p className="text-black text-18 font-semibold leading-24">
-                            {navInfo.name}
-                          </p>
-                        </NavLink>
-                      ) : (
-                        <div></div>
-                      )}
-                    </div>
-                  );
-                } else {
-                  return <div key={navInfo.name}></div>;
-                }
+              {navDropDown.map((navInfo) => {
+                return (
+                  <div>
+                    {isExpanded ? (
+                      <NavLink
+                        key={navInfo.name}
+                        to={navInfo.path}
+                        className="w-full h-48 px-20 flex items-center bg-white rounded-8"
+                      >
+                        <p className="text-black text-18 font-semibold leading-24">
+                          {navInfo.name}
+                        </p>
+                      </NavLink>
+                    ) : (
+                      <div></div>
+                    )}
+                  </div>
+                );
               })}
             </div>
           ) : (
