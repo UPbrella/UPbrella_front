29706477eeb5fa62436eeb91e839dfc7ae503c7e
heehminh <mink141416@gmail.com>
2023-09-27 12:13:37 +0900

    feat: 이용약관, 개인정보처리방침 연결

--- src/components/molecules/Footer/FooterLabel/index.tsx
+++ src/components/molecules/Footer/FooterLabel/index.tsx
@@ -4,8 +4,8 @@ const FooterLabel = () => {
   const navigate = useNavigate();
   return (
     <div className="flex gap-6 text-sm text-gray-700">
-      <button>이용약관</button>
-      <button>개인정보처리방침</button>
+      <button onClick={() => navigate("/info/tos")}>이용약관</button>
+      <button onClick={() => navigate("/info/pp")}>개인정보처리방침</button>
       <button className="font-semibold" onClick={() => navigate("/contact")}>
         CONTACT_US
       </button>
