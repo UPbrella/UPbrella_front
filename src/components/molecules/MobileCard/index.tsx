4fb51f2f91b1d851ed665a9edf91b89a42772715
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-10-05 23:14:38 +0900

    feat: 바텀 모바일 카드에 버튼 추가

--- src/components/molecules/MobileCard/index.tsx
+++ src/components/molecules/MobileCard/index.tsx
@@ -1,29 +1,31 @@
 import MobileCardInfo from "@/components/atoms/MobileCardInfo";
 import NaverDirectionBtn from "@/components/atoms/NaverDirectionBtn";
 import { TStoreListDetail } from "@/types/admin/StoreTypes";
+import DetailBtn from "@/components/atoms/DetailBtn";
 
 export type TMobileCard = {
   storeDetail: TStoreListDetail;
 };
 
-const MobileCard = (storeDetail: TMobileCard) => {
+const MobileCard = ({ storeDetail }: TMobileCard) => {
   return (
     <div className="mb-20 px-20 bg-white">
       <div className="flex justify-center items-center mb-16">
-        <div className="text-gray-700 text-16 font-semibold mr-8">
-          {storeDetail.storeDetail.name}
-        </div>
-        <div className="text-gray-600 text-12">{storeDetail.storeDetail.category}</div>
+        <div className="text-gray-700 text-16 font-semibold mr-8">{storeDetail.name}</div>
+        <div className="text-gray-600 text-12">{storeDetail.category}</div>
       </div>
       <div className="flex gap-x-2 mb-16">
-        <img className="rounded-8 w-120 h-120" src={storeDetail.storeDetail.imageUrls[0]}></img>
-        <MobileCardInfo storeDetail={storeDetail.storeDetail} />
+        <img className="rounded-8 w-120 h-120" src={storeDetail.imageUrls[0]}></img>
+        <MobileCardInfo storeDetail={storeDetail} />
       </div>
-      <NaverDirectionBtn
-        elon={storeDetail.storeDetail.longitude}
-        elat={storeDetail.storeDetail.latitude}
-        address={storeDetail.storeDetail.address}
-      />
+      <div className="grid grid-cols-2 gap-4">
+        <DetailBtn id={storeDetail.id} />
+        <NaverDirectionBtn
+          elon={storeDetail.longitude}
+          elat={storeDetail.latitude}
+          address={storeDetail.address}
+        />
+      </div>
     </div>
   );
 };
