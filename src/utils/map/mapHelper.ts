da9a673f714ea1b4aaf233d73b47d1466e547cde
Clay ChanWoo <claychanwoo@naver.com>
2023-10-10 23:08:40 +0900

    fix: 대여소 위치 페이지 ux 개선

--- src/utils/map/mapHelper.ts
+++ src/utils/map/mapHelper.ts
@@ -1,7 +1,11 @@
 export const getUserPosition = (): Promise<GeolocationPosition> => {
   return new Promise((resolve, reject) => {
     if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(resolve, reject);
+      navigator.geolocation.getCurrentPosition(resolve, reject, {
+        enableHighAccuracy: false,
+        timeout: 5000,
+        maximumAge: Infinity,
+      });
     } else {
       reject(new Error("Geolocation not available"));
     }
