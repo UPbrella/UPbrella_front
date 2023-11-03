fe619333a137c7c87691abb589ff60f5d2a36241
LeeJSYS <dauzzang@gmail.com>
2023-11-03 17:31:19 +0900

    design: lg사이즈에서 폰트크기, 마진 크기 수정

--- src/components/molecules/Mypage/MypageModalChildren/index.tsx
+++ src/components/molecules/Mypage/MypageModalChildren/index.tsx
@@ -4,13 +4,15 @@ export type MypageModalChildrenProps = {
 };
 const MypageModalChildren = ({ label, onClickBtn }: MypageModalChildrenProps) => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="text-gray-700 text-24 font-semibold leading-32 mb-24">{label}</div>
+    <div className="flex flex-1 flex-col justify-center items-center">
+      <div className="text-gray-700 text-24 font-semibold leading-32 mb-24 lg:mb-16 lg:text-18">
+        {label}
+      </div>
       <button
         className="w-160 h-56 px-32 flex justify-center items-center bg-primary-500 rounded-8"
         onClick={onClickBtn}
       >
-        <p className="text-white text-18 font-semibold leading-24">확인</p>
+        <p className="text-white text-18 font-semibold leading-24 lg:text-16">확인</p>
       </button>
     </div>
   );
