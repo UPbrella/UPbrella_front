a8d8ffee9aa6e0a4a35009835acf9af50918a72e
gwonwoo-nam <ngw7617@naver.com>
2025-05-23 23:37:33 +0900

    fix: 개발서버 설정

--- vite.config.ts
+++ vite.config.ts
@@ -20,9 +20,10 @@ export default ({ mode }) => {
       }),
     ],
     server: {
+      port: 3000,
       proxy: {
         "/api": {
-          target: "https://api.upbrella.co.kr",
+          target: env.VITE_UPBRELLA_API_BASE_URL,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
