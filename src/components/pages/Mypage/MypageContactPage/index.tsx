f42a6b95ba7a92237211c22acb39a5c5d692850c
Clay ChanWoo <claychanwoo@naver.com>
2026-03-27 00:59:07 +0900

    [chore] 레거시 파일 정리 - Storybook, stories, 미사용 파일 제거
    - Storybook 설정 파일(.storybook/) 및 기본 stories(src/stories/) 제거
    - 컴포넌트 stories 파일 76개 일괄 삭제 (FSD 전환 후 불필요)
    - Docker/nginx 설정 파일 제거 (Vercel 배포로 전환 완료)
    - 미사용 페이지 삭제: OfflinePage, TempPage, MypageContactPage
    - 미사용 타입/API 삭제: userTypes, ContactTypes, MypageTypes, StoresTypes, clientUserApi
    - 기타: instagram.svg, NaverMap 레거시 컴포넌트 제거

--- src/components/pages/Mypage/MypageContactPage/index.tsx
+++
@@ -1,7 +0,0 @@
-import MypageContactCard from "@/components/organisms/Mypage/MypageContactCard";
-import MypageLayout from "@/components/pages/Mypage/MypageLayout";
-
-const MypageContactPage = () => {
-  return <MypageLayout renderChildren={() => <MypageContactCard />} />;
-};
-export default MypageContactPage;
